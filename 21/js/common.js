4462997f229c68c9c5d9851e90142413dd506678
testishe <32316231+testishe@users.noreply.github.com>
2017-12-20 11:19:19 +0500

    48

--- 21/js/common.js
+++ 21/js/common.js
@@ -12,29 +12,29 @@ $(function() {
 		$(".nav-wrap").toggleClass("open");
 	});
 
-	$(".slider1__content").owlCarousel({
-	items: 1,
-	nav: false,
-	navText: [ , ],
-	dots: true,
-	// autoplay: true,
-	autoplayTimeout: 3000,
-	autoplayHoverPause: true,
-	loop: true,
-	mouseDrag: true
-	});
+	// $(".slider1__content").owlCarousel({
+	// items: 1,
+	// nav: false,
+	// navText: [ , ],
+	// dots: true,
+	// // autoplay: true,
+	// autoplayTimeout: 3000,
+	// autoplayHoverPause: true,
+	// loop: true,
+	// mouseDrag: true
+	// });
 
-	$(".slider2__container").owlCarousel({
-	items: 1,
-	nav: false,
-	navText: [ , ],
-	dots: true,
-	// autoplay: true,
-	autoplayTimeout: 3000,
-	autoplayHoverPause: true,
-	loop: true,
-	mouseDrag: true
-	});
+	// $(".slider2__container").owlCarousel({
+	// items: 1,
+	// nav: false,
+	// navText: [ , ],
+	// dots: true,
+	// // autoplay: true,
+	// autoplayTimeout: 3000,
+	// autoplayHoverPause: true,
+	// loop: true,
+	// mouseDrag: true
+	// });
 
 	$('a[href^="#"]').click(function (){
 	var elementClick = $(this).attr("href");
@@ -48,4 +48,169 @@ $(function() {
 		mainClass: 'mfp-fade'
 	});
 
+
+
+	// $(".sl-item").owlCarousel({
+	// items: 1,
+	// nav: false,
+	// navText: [ , ],
+	// dots: true,
+	// // autoplay: true,
+	// autoplayTimeout: 3000,
+	// autoplayHoverPause: true,
+	// loop: true,
+	// mouseDrag: true
+	// });
+
+
+
+
+	$('.js-link-popup1').magnificPopup({
+	    type: 'inline',
+	    preloader: false,
+	    removalDelay: 350,
+		mainClass: 'mfp-fade',
+	    callbacks: {
+	      open: function() {
+	        // Will fire when this exact popup is opened
+	        // this - is Magnific Popup object
+	        $(".sl-item1").owlCarousel({
+				items: 1,
+				nav: true,
+				navText: ['<','>'],
+				dots: true,
+				// autoplay: true,
+				autoplayTimeout: 3000,
+				autoplayHoverPause: true,
+				loop: true,
+				mouseDrag: true
+			});
+	      },
+	      close: function() {
+	        // Will fire when popup is closed
+	      }
+	      // e.t.c.
+	    }
+	    // modal: true
+	});
+
+	$('.js-link-popup2').magnificPopup({
+	    type: 'inline',
+	    preloader: false,
+	    removalDelay: 350,
+		mainClass: 'mfp-fade',
+	    callbacks: {
+	      open: function() {
+	        // Will fire when this exact popup is opened
+	        // this - is Magnific Popup object
+	        $(".sl-item2").owlCarousel({
+				items: 1,
+				nav: true,
+				navText: ['<','>'],
+				dots: true,
+				// autoplay: true,
+				autoplayTimeout: 3000,
+				autoplayHoverPause: true,
+				loop: true,
+				mouseDrag: true
+			});
+	      },
+	      close: function() {
+	        // Will fire when popup is closed
+	      }
+	      // e.t.c.
+	    }
+	    // modal: true
+	});
+
+	$('.js-link-popup3').magnificPopup({
+	    type: 'inline',
+	    preloader: false,
+	    removalDelay: 350,
+		mainClass: 'mfp-fade',
+	    callbacks: {
+	      open: function() {
+	        // Will fire when this exact popup is opened
+	        // this - is Magnific Popup object
+	        $(".sl-item3").owlCarousel({
+				items: 1,
+				nav: true,
+				navText: ['<','>'],
+				dots: true,
+				// autoplay: true,
+				autoplayTimeout: 3000,
+				autoplayHoverPause: true,
+				loop: true,
+				mouseDrag: true
+			});
+	      },
+	      close: function() {
+	        // Will fire when popup is closed
+	      }
+	      // e.t.c.
+	    }
+	    // modal: true
+	});
+
+	$('.js-link-popup4').magnificPopup({
+	    type: 'inline',
+	    preloader: false,
+	    removalDelay: 350,
+		mainClass: 'mfp-fade',
+	    callbacks: {
+	      open: function() {
+	        // Will fire when this exact popup is opened
+	        // this - is Magnific Popup object
+	        $(".sl-item4").owlCarousel({
+				items: 1,
+				nav: true,
+				navText: ['<','>'],
+				dots: true,
+				// autoplay: true,
+				autoplayTimeout: 3000,
+				autoplayHoverPause: true,
+				loop: true,
+				mouseDrag: true
+			});
+	      },
+	      close: function() {
+	        // Will fire when popup is closed
+	      }
+	      // e.t.c.
+	    }
+	    // modal: true
+	});
+
+	$('.js-link-popup5').magnificPopup({
+	    type: 'inline',
+	    preloader: false,
+	    removalDelay: 350,
+		mainClass: 'mfp-fade',
+	    callbacks: {
+	      open: function() {
+	        // Will fire when this exact popup is opened
+	        // this - is Magnific Popup object
+	        $(".sl-item5").owlCarousel({
+				items: 1,
+				nav: true,
+				navText: ['<','>'],
+				dots: true,
+				// autoplay: true,
+				autoplayTimeout: 3000,
+				autoplayHoverPause: true,
+				loop: true,
+				mouseDrag: true
+			});
+	      },
+	      close: function() {
+	        // Will fire when popup is closed
+	      }
+	      // e.t.c.
+	    }
+	    // modal: true
+	});
+
 });
+
+
+
