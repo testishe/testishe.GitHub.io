e9453bda7b3205906621fbf1d31aafa5ab2bbca3
testishe <32316231+testishe@users.noreply.github.com>
2018-06-11 22:23:05 +0500

    74

---
+++ 31/bitrix/cache/js/s1/aspro_mshop/page_9a41604dd4898941569a5ea99582d868/page_9a41604dd4898941569a5ea99582d868f2e1.js
@@ -0,0 +1,7 @@
+
+; /* Start:"a:4:{s:4:"full";s:94:"/bitrix/templates/aspro_mshop/components/bitrix/catalog.element/landing/script.js?152455765395";s:6:"source";s:81:"/bitrix/templates/aspro_mshop/components/bitrix/catalog.element/landing/script.js";s:3:"min";s:0:"";s:3:"map";s:0:"";}"*/
+$(document).on('click', ".scroll_btn", function(){
+	scroll_block($('#right_block_ajax'));
+});
+/* End */
+;; /* /bitrix/templates/aspro_mshop/components/bitrix/catalog.element/landing/script.js?152455765395*/
---
+++ 31/company/news/rss.php
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="windows-1251"?><rss version="2.0">
+<channel>
+<title>Новости</title>
+<link>http://market.aspro-demo.ru</link>
+<description></description>
+<lastBuildDate>Sat, 13 Jan 2018 15:17:12 +0300</lastBuildDate>
+<ttl>60</ttl>
+</channel>
+</rss>
