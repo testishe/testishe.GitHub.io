a049dafea79e84603eb54ee24ea0ad4efbc32486
testishe <32316231+testishe@users.noreply.github.com>
2018-06-14 19:25:15 +0500

    113

--- 31/bitrix/templates/aspro_mshop/js/custom.js
+++ 31/bitrix/templates/aspro_mshop/js/custom.js
@@ -13,9 +13,6 @@ $(document).ready(function(){
 
 var counterBasket = 0;
 $(".opener").click(function(){
-
-
-	
 	console.log(counterBasket);
 	if(counterBasket == 0) {
 		$("#basket_line .basket_fly").css({ "right": "0", "transition":".5s"})
@@ -26,5 +23,40 @@ $(".opener").click(function(){
 		counterBasket = 0;
 	}
 });
+$("#basket_line .close").click(function(){
+	$("#basket_line .basket_fly").css({ "right": "-805px"})
+	counterBasket = 0;
+});
+
+
+
+$(".close").click(function(){
+	$(".mainBg").fadeOut();
+	$(".mainPopup").fadeOut();
+});
+
+$(".mainBg").click(function(){
+	$(".mainBg").fadeOut();
+	$(".mainPopup").fadeOut();
+});
+
+$(".callback_btn").click(function(){
+	$(".mainBg").fadeIn();
+	$(".mainPopup1").fadeIn();
+});
+$(".mainPopup1 button[type='reset']").click(function(){
+	$(".mainBg").fadeOut();
+	$(".mainPopup1").fadeOut();
+});
+
+$(".avtorization-call").click(function(){
+	$(".mainBg").fadeIn();
+	$(".mainPopup2").fadeIn();
+});
+$(".mainPopup2 button[type='reset']").click(function(){
+	$(".mainBg").fadeOut();
+	$(".mainPopup2").fadeOut();
+});
+
 
 });
