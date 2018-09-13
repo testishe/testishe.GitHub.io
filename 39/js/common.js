cabf1152943520ba42ee56c94cfee640bf75029c
testishe <32316231+testishe@users.noreply.github.com>
2018-09-13 22:51:11 +0500

    shop

---
+++ 39/js/common.js
@@ -0,0 +1,11 @@
+$(function() {
+
+	// Custom JS
+	$('.slick').slick({
+		arrows: true,
+		dots: true,
+		prevArrow: '<div type="button" class="prev-ar"></div>',
+		nextArrow: '<div type="button" class="next-ar"></div>'
+	});
+
+});
